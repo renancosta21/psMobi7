07ae2e488f66728700c154ea8ba0719122075f54
Renan Costa <renancosta21@gmail.com>
2022-02-21 17:55:27 -0300

    rename poisModel variables

--- models/poisModel.js
+++ models/poisModel.js
@@ -2,9 +2,9 @@ const connection = require('../connection');
 
 const getAllPois = async () => {
   const db = await connection();
-  const positions = await db.collection('pois').find({})
+  const pois = await db.collection('pois').find({})
       .toArray();
-  return positions;
+  return pois;
 };
 
 module.exports = {getAllPois};
