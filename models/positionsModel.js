15a4c94591069b555c04c156a737121451b11273
Renan Costa <renancosta21@gmail.com>
2022-02-21 17:30:06 -0300

    fix: collection name changed to 'positions'

--- models/positionsModel.js
+++ models/positionsModel.js
@@ -2,7 +2,7 @@ const connection = require('../connection');
 
 const getAllPositions = async () => {
   const db = await connection();
-  const positions = await db.collection('posicoes').find({})
+  const positions = await db.collection('positions').find({})
       .toArray();
   return positions;
 };
