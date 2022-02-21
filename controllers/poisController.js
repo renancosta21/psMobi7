b836b4aa6e59167ac512e94add543623c893e67c
Renan Costa <renancosta21@gmail.com>
2022-02-21 18:09:44 -0300

    fix: function name

--- controllers/poisController.js
+++ controllers/poisController.js
@@ -3,7 +3,7 @@ const HTTP_OK_STATUS = 200;
 const poisModel = require('../models/poisModel');
 
 const getAllPois = async (_req, res) => {
-  const pois = await poisModel.getAllPositions();
+  const pois = await poisModel.getAllPois();
 
   res.status(HTTP_OK_STATUS).json(pois);
 };
